8811c27cf8aaa73868ef73220874a8e8602fac93
Jrnicus <jrnicus@gmail.com>
2019-10-08 14:58:16 -0400

    changed the bounce method
    The bounce method now need a parameter given by the user to decide how many ball will be bouncing. The balls are then randomly generated with diffrent sizes, colors, and starting postions.

--- BallDemo.java
+++ BallDemo.java
@@ -1,19 +1,22 @@
 import java.awt.Color;
-
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
  *
- * @author Bill Crosbie
- * @version 2015-March-BB
+ * @author Steve Cate
+ * @version 210/08/2019
  *
  * @author Michael Kölling and David J. Barnes
- * @version 2011.07.31
  */
 
 public class BallDemo   
 {
     private Canvas myCanvas;
+    private Random rand;
+    private ArrayList<Color> colors;
+    private ArrayList<BouncingBall> balls;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -21,12 +24,22 @@ public class BallDemo
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+        rand = new Random();
+        colors = new ArrayList<Color>();
+        colors.add(Color.RED);
+        colors.add(Color.BLUE);
+        colors.add(Color.GREEN);
+        colors.add(Color.ORANGE);
+        colors.add(Color.PINK);
+        balls = new ArrayList<BouncingBall>();
     }
 
     /**
-     * Simulate two bouncing balls
+     * Simulate bouncing balls the number of balls bouncing
+     * is given by the user and the balls are then radnomly generated
+     * @param numberOfBalls the number of balls you want to display
      */
-    public void bounce()
+    public void bounce(int numberOfBalls)
     {
         int ground = 400;   // position of the ground line
 
@@ -36,20 +49,33 @@ public class BallDemo
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
+        int i = 0;
+        while (i < numberOfBalls)
+        {   
+            balls.add(new BouncingBall (rand.nextInt(300)+50, rand.nextInt(400), rand.nextInt(40)+5, 
+                    colors.get(rand.nextInt(5)), ground, myCanvas));
+                    
+            balls.get(i).draw();
+            i++;
+        }
 
         // make them bounce
         boolean finished =  false;
-        while(!finished) {
+        while(!finished) 
+        {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
+            
+            for(BouncingBall ball : balls)
+            {
+                ball.move();
+            }
             // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            for(BouncingBall ball : balls)
+            {    
+                if(ball.getXPosition() >= 550) 
+                {
+                    finished = true;
+                }
             }
         }
     }
